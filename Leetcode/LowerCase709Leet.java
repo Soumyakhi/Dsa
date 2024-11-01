6fcbd176b7e2f67b4fe70b0eef05a3751b29967c
Soumyakhi <111576586+Soumyakhi@users.noreply.github.com>
2024-11-02 02:23:35 +0530

    Delete Leetcode directory

--- Leetcode/LowerCase709Leet.java
+++
@@ -1,8 +0,0 @@
-public class LowerCase709Leet {
-    public String toLowerCase(String s) {
-        return s.toLowerCase();
-    }
-    public static void main(String[] args) {
-        System.out.println((new LowerCase709Leet()).toLowerCase("Hello"));
-    }
-}
